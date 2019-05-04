0bfaa775861c9ec964942e7acba248a7d7f99861
Jascha <jascha@jaeh.at>
2019-05-04 23:40:47 +0200

    add propTypes, overload defaults with props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-const ClientList = () =>
+const ClientList = props =>
   GitList({
     id: 'magic-client',
     org: 'magic-client',
-    prefix: 'client',
-    header: '@magic-client libraries',
+    header: [Link({ to: 'https://magic-client.github.io' }, '@magic-client libraries')],
     desc: [
       'below is a collection of the available @magic client libraries.',
     ],
@@ -20,10 +19,19 @@ const ClientList = () =>
         description: 'the @magic-client/is module unifies the javascript type testing apis.',
       },
     ],
+    ...props,
   })
 
 ClientList.dependencies = {
   GitList: require('@magic-modules/git-list'),
 }
+
+ClientList.props = [
+  { key: 'id', type: 'string' },
+  { key: 'org', type: 'string' },
+  { key: 'header', type: 'string' },
+  { key: 'desc', type: ['string', 'array'] },
+  { key: 'items', type: 'array', required: true },
+]
 
 module.exports = ClientList
