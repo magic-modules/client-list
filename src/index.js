4b8c7aeb255720b62327451bc7e6f7cd902623e6
Jascha <jascha@jaeh.at>
2019-05-04 23:34:46 +0200

    use git list

--- src/index.js
+++ src/index.js
@@ -1,45 +1,29 @@
 const ClientList = () =>
-  ul({ class: 'ClientList' }, [
-    li([
-      h3(
-        { id: 'client-libs-json' },
-        Link({ to: 'https://github.com/magic-client/json' }, '@magic-client/json'),
-      ),
-      div([
-        'the @magic-client/json module parses and stringifies json.',
-        ' it will also return errors instead of throwing them.',
-      ]),
-      GitBadges({
-        project: 'magic-client/json',
-        appveyor: 'jaeh/json',
-      }),
-      Link({ to: 'https://magic-client.github.io/json' }, 'docs / demo'),
-    ]),
-    li([
-      h3(
-        { id: 'client-libs-is' },
-        Link({ to: 'https://github.com/magic-client/is/' }, '@magic-client/is'),
-      ),
-      div('the @magic-client/is module unifies the javascript type testing apis.'),
-      GitBadges({
-        project: 'magic-client/is',
-        appveyor: 'jaeh/is',
-      }),
-      Link({ to: 'https://magic-client.github.io/is/' }, 'docs / demo'),
-    ]),
-  ])
-
-ClientList.style = {
-  '.ClientList': {
-    a: {
-      display: 'block',
-      lineHeight: 1.8,
-    },
-  },
-}
+  GitList({
+    id: 'magic-client',
+    org: 'magic-client',
+    prefix: 'client',
+    header: '@magic-client libraries',
+    desc: [
+      'below is a collection of the available @magic client libraries.',
+    ],
+    items: [
+      {
+        name: 'json',
+        description: [
+          'the @magic-client/json module parses and stringifies json.',
+          ' it will also return errors instead of throwing them.',
+        ],
+      },
+      {
+        name: 'is',
+        description: 'the @magic-client/is module unifies the javascript type testing apis.',
+      },
+    ],
+  })
 
 ClientList.dependencies = {
-  GitBadges: require('@magic-modules/git-badges'),
+  GitList: require('@magic-modules/git-list'),
 }
 
 module.exports = ClientList
