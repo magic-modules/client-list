fc85e32b8da5d2ecce2d9e02fd75e17b498af140
Jascha <jascha@jaeh.at>
2019-05-20 07:14:59 +0200

    format

--- test/spec.mjs
+++ test/spec.mjs
@@ -2,5 +2,9 @@ import { is } from '@magic/test'
 import * as LibraryList from '../src/index.mjs'
 
 export default [
-  { fn: () => LibraryList.View, expect: is.function, info: 'expect LibraryList.View to be a function' },
+  {
+    fn: () => LibraryList.View,
+    expect: is.function,
+    info: 'expect LibraryList.View to be a function',
+  },
 ]
