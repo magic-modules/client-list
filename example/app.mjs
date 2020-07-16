c9d1504fbe0b7d0c3605fa3fadfa8d939c8487dd
jascha <jascha@jaeh.at>
2020-07-16 23:16:47 +0200

    update logotext

--- example/app.mjs
+++ example/app.mjs
@@ -1,7 +1,6 @@
 export const state = {
   title: '@magic-modules/library-list',
   description: 'shows a list of all @magic-libraries with gitbadges and urls to the docs',
-
   logotext: 'LibraryList',
 
   menu: [
